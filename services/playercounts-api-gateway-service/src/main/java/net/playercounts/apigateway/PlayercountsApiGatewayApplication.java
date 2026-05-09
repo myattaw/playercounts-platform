04dae7c231ae40adb6919620b6856175c43b8cdd
myattaw <39500954+myattaw@users.noreply.github.com>
2026-05-09 12:12:54 -0400

    Add Swagger/OpenAPI support and fixed entity scan for user

--- services/playercounts-api-gateway-service/src/main/java/net/playercounts/apigateway/PlayercountsApiGatewayApplication.java
+++ services/playercounts-api-gateway-service/src/main/java/net/playercounts/apigateway/PlayercountsApiGatewayApplication.java
@@ -3,10 +3,15 @@ package net.playercounts.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "net.playercounts")
-@EntityScan(basePackages = "net.playercounts.models")
+@EntityScan(basePackages = {
+        "net.playercounts.models",
+        "net.playercounts.apigateway.entity"
+})
+@EnableJpaRepositories(basePackages = "net.playercounts.apigateway.repository")
 @EnableScheduling
 public class PlayercountsApiGatewayApplication {
 
---
+++ services/playercounts-api-gateway-service/src/main/java/net/playercounts/apigateway/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package net.playercounts.apigateway.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "PlayerCounts API",
+                version = "1.0",
+                description = "Telemetry, authentication, and admin API for PlayerCounts"
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
